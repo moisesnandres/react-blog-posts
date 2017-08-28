4589ecb6891274a4911c48a86b720d7ede29fc60
Moisés Ñañez <moisesnandres@gmail.com>
2017-08-27 22:24:26 -0500

    Add action to create post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
 export const FETCH_POST = 'FETCH_POST';
+export const CREATE_POST = 'CREATE_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=moises'
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-  
+
   return {
     type: FETCH_POST,
+    payload: request
+  }
+}
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
